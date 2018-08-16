bd3dfdb4d2fcad5d1379ce88de63951e5ae11a07
Madeline Stark <madeline.stark1@gmail.com>
2018-08-16 23:23:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,12 +67,12 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE name = ?
-    LIMIT 1
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
     SQL
-    binding.pry
+    
     DB[:conn].execute(sql, name).map do |row| #by using name as second argument, it’s replacing the ? in where and only returning those values
         self.new_from_db(row)
     end.first #chaining .first to return first element of the returned array
