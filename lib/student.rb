7cf3318cd61576f6a19a91c93d5adf2cda0b1212
Madeline Stark <madeline.stark1@gmail.com>
2018-08-16 22:48:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,30 +27,31 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
-
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0] #grabs id #
-
-  end
-
   # def save
-  #     if self.id
-  #         self.update
-  #     else
-  #         sql = <<-SQL
-  #         INSERT INTO students (name, grade) VALUES (?, ?)
-  #         SQL
-  #         DB[:conn].execute(sql, self.name, self.grade)
-  #         @id = DB[:conn].execute(“SELECT last_insert_rowid() FROM students”)[0][0]
-  #     end
+  #   sql = <<-SQL
+  #     INSERT INTO students (name, grade)
+  #     VALUES (?, ?)
+  #   SQL
+  # 
+  #   DB[:conn].execute(sql, self.name, self.grade)
+  # 
+  #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0] #grabs id #
+  # 
   # end
-  #
+
+  def save
+      if self.id
+          self.update
+      else
+          sql = <<-SQL
+          INSERT INTO students (name, grade) 
+          VALUES (?, ?)
+          SQL
+          DB[:conn].execute(sql, self.name, self.grade)
+          @id = DB[:conn].execute(“SELECT last_insert_rowid() FROM students”)[0][0]
+      end
+  end
+  
   def update
         sql = <<-SQL
         UPDATE students
